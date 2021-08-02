73aafd72d60aaa5290f1ea141759efd7d3484ff8
Hubert Brzostowski <hubertbrzostowski@Huberts-MacBook-Pro.local>
2021-08-02 02:00:28 +0200

    More..

--- icu.rb
+++ icu.rb
@@ -14,8 +14,7 @@ class Icu < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "cd", "source"
-    system "./configure", "--prefix=#{prefix}"
+    system "./source/configure", "--prefix=#{prefix}"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "make", "install"
   end
